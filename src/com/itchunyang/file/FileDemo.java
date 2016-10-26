badaeb5dd470f7c942c13a51fd837fb71b610ab1
luchunyang <itchunyang@gmail.com>
2016-10-26 09:59:17 +0800

    Initial commit

---
+++ src/com/itchunyang/file/FileDemo.java
@@ -0,0 +1,13 @@
+package com.itchunyang.file;
+
+/**
+ * Created by luchunyang on 2016/10/26.
+ */
+public class FileDemo {
+
+    //快捷键psvm
+    public static void main(String[] args) {
+
+    }
+    
+}
